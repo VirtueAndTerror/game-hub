a8a783b4351e51fcc5a36544b7dbaeb82ca00d5b
Arnaldo <arnaldo.e.diaz@gmail.com>
2024-06-14 02:22:45 -0400

    Refactoring remove duplicate interfaces

--- src/hooks/useGenres.ts
+++ src/hooks/useGenres.ts
@@ -1,3 +1,6 @@
+import { useQuery } from '@tanstack/react-query';
+import apiClient from '../services/api-client';
+import { FetchResponse } from '../services/api-client';
 import genres from '../data/genres';
 
 export interface Genre {
@@ -9,6 +12,15 @@ export interface Genre {
 // const useGenres = () => useData<Genre>('/genres');
 
 // static data
-const useGenres = () => ({ data: genres, isLoading: false, error: null });
+// const useGenres = () => ({ data: genres, isLoading: false, error: null });
 
+const useGenres = () =>
+  useQuery({
+    queryKey: ['genres'],
+    queryFn: () =>
+      apiClient.get<FetchResponse<Genre>>('/genres').then((res) => res.data),
+    // Data will be fresh for 24hs, i.e., no request will be made to the backend to fetch the genres
+    staleTime: 24 * 60 * 60 * 1000, // 24hs
+    initialData: { count: genres.length, results: genres },
+  });
 export default useGenres;
--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -1,8 +1,13 @@
-import axios from "axios";
+import axios from 'axios';
+
+export interface FetchResponse<T> {
+  count: number;
+  results: T[];
+}
 
 export default axios.create({
-    baseURL: "https://api.rawg.io/api",
-    params: {
-        key: '4416ca5a48454d9d8bbd04afe0f61c00'
-    }
-})
+  baseURL: 'https://api.rawg.io/api',
+  params: {
+    key: '4416ca5a48454d9d8bbd04afe0f61c00',
+  },
+});
